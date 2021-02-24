6ca95618726ae46fbf23ceecf2dc86d186d7b8c2
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2021-02-24 10:54:09 -0700

    Bump version number
    No new changes made. Need to update the existing version number so that it properly indicates no backwards compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, SparkFun Electronics'
 author = 'SparkFun Electronics'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
